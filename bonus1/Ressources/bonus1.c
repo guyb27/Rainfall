18a4ec2a0de8039b65ec6eb70d109dc69b9f9eea
guyb27 <guillaume.madec@protonmail.com>
2022-04-26 12:52:40 -0400

    bonus1 finish

--- bonus1/Ressources/bonus1.c
+++ bonus1/Ressources/bonus1.c
@@ -21,7 +21,7 @@ int main(int ac, char **av)//av[1], av[2]
 //Si on met -1 en premier argument, on obtient 0xfffffffc en troisieme argument de memcpy, et 0xfffffff8 si on met -2.
 //On peut en deduire que nous demarrons de 4294967295 et que chaque unite negative decremente ce nombre de 4.
 
-//Nous avone une vulnerabilite de conversion de signe, Type Demotion/Narrowing ou de int min (je pense plus a une conversion de signe par rapport au ), comme indiquer sur cette page:
+//Nous avone une vulnerabilite de conversion de signe, Type Demotion/Narrowing ou de int min (je pense plus a une conversion de signe par rapport au bit de signe), comme indiquer sur cette page:
 //https://blog.feabhas.com/2014/10/vulnerabilities-in-c-when-integers-go-bad/#What_are_the_potential_underlying_problems
 //maxInt: 2 147 483 647
 //(gdb) p/d 0xc000000c
