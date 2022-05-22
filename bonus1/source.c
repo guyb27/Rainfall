a03f2390b639609caf5e7a5a2c41b81ba54f1f48
guyb27 <guillaume.madec@protonmail.com>
2022-05-22 05:47:15 -0400

    Source bonus1 finish

--- bonus1/source.c
+++ bonus1/source.c
@@ -1,6 +1,7 @@
 
 int main(int argc, char *argv[]) {
 	int n;
+	int res;
 	char buf[40];
 
 	n = atoi(argv[1]);
@@ -9,7 +10,7 @@ int main(int argc, char *argv[]) {
 
 	memcpy(buf, argv[2], n * 4);
 
-	if (n == 1464814662) {
+	if (res == 1464814662) {
 		execl("/bin/sh", "sh", 0);
 	}
 
